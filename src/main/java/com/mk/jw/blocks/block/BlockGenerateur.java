591a9593a10ad997ad8079b153c67b502cee13f9
MK <florentmarc6@gmail.com>
2021-08-03 16:23:45 +0200

    Patch BlockGenerateur

--- src/main/java/com/mk/jw/blocks/block/BlockGenerateur.java
+++ src/main/java/com/mk/jw/blocks/block/BlockGenerateur.java
@@ -1,29 +1,36 @@
-package com.mk.jw.blocks;
+package com.mk.jw.blocks.block;
 
 import com.jme3.math.Vector3f;
+import com.mk.jw.items.ItemBidon;
 import com.mk.jw.blocks.tile.TEGenerateur;
+import com.mk.jw.juralink.ElectricityLink;
 import com.mk.jw.jw;
 import fr.dynamx.common.blocks.DynamXBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import org.jline.utils.Log;
 
 import javax.annotation.Nullable;
 
 
-public class Generateur extends DynamXBlock  {
+public class BlockGenerateur extends DynamXBlock  {
 
-    public Generateur(Material material, String modid, String blockName, String model) {
+    public BlockGenerateur(Material material, String modid, String blockName, String model) {
         super(material, modid, blockName, model);
         this.setCreativeTab(jw.juraTabs);
         this.blockObjectInfo.setTranslation(new Vector3f(0.0F, -1.5F, 0.0F));
     }
+
+
 
 
     public int getHarvestLevel(IBlockState state) {
@@ -39,11 +46,20 @@ public class Generateur extends DynamXBlock  {
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         boolean hasCanister = playerIn.getHeldItemMainhand().getItem() == jw.bidon;
+        ItemStack Canister = playerIn.getHeldItemMainhand();
         if (!worldIn.isRemote) {
             if (hasCanister) {
                 TEGenerateur te = (TEGenerateur) worldIn.getTileEntity(pos);
-                te.addfuel(te.getFuel() + 10);
-                bidon.setfuel(playerIn.getHeldItemMainhand(), bidon.getfuel(playerIn.getHeldItemMainhand()) - 10);
+                if(ItemBidon.getfuel(Canister)>=10) {
+                    if ((te.getFuel() + 10) <= te.getCapacitor()) {
+                        te.addfuel(10);
+                        ItemBidon.setfuel(Canister, ItemBidon.getfuel(Canister) - 10);
+                    }else{
+                        playerIn.sendMessage(new TextComponentString("Geneteur full"));
+                    }
+                }else{
+                    playerIn.sendMessage(new TextComponentString("Bidon pas assez rempli"));
+                }
             }
         }else{
             if(!hasCanister){
@@ -53,5 +69,15 @@ public class Generateur extends DynamXBlock  {
         }
 
         return true;
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        ElectricityLink.addlist(pos);
+    }
+
+    @Override
+    public void onPlayerDestroy(World worldIn, BlockPos pos, IBlockState state) {
+        ElectricityLink.checklist(pos);
     }
 }
